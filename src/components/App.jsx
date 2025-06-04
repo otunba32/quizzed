99e2ddce7cf38d9e8319df624511d14f8d259401
Emmanuel Abia <emmanuel.abia32@gmail.com>
2025-06-03 23:21:57 -0400

    Finishing the quiz

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import StartScreen from "./startScreen";
 import Questions from "./Questions";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishedScreen from "./FinishedScreen";
 
 const initialState = {
   questions: [],
@@ -16,6 +17,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0
 };
 
 function reducer(state, action) {
@@ -49,13 +51,15 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+      case "finish":
+      return { ...state, status: "finished", highscore: state.points > state.highscore ? state.points: state.highscore };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -98,8 +102,20 @@ export default function App() {
                 dispatch={dispatch}
                 answer={answer}
               />
-              <NextButton dispatch={dispatch} answer={answer} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={numQuestions}
+                index={index}
+              />
             </>
+          )}
+          {status === "finished" && (
+            <FinishedScreen
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              highscore={highscore}
+            />
           )}
         </Main>
       </div>
